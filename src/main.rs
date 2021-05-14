5bfb80ff3d68c59aa49914dc4b792dd6ae92c869
Salva HG <salva.hg01@gmail.com>
2021-05-14 16:17:24 -0500

    Add basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod endpoints;
+
+use actix_web::{App, HttpServer};
+
+use endpoints::*;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
