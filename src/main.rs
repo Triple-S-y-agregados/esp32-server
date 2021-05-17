12461bec57aefbab5848f26e62a70d701088a571
Salva HG <salva.hg01@gmail.com>
2021-05-17 05:24:22 -0500

    Add cors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod endpoints;
 
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 
 use endpoints::*;
@@ -7,7 +8,14 @@ use endpoints::*;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::default()
+              .allow_any_origin()
+              .allow_any_header()
+              .allow_any_method()
+              .max_age(3600);
+
         App::new()
+            .wrap(cors)
             .service(voltage)
             .service(records)
             .service(get_records)
